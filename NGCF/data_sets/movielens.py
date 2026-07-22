7f4e5e89681498e1c63a861db764806304828c7a
Michal Mrowicki <mrg.mrowicki@gmail.com>
2026-07-22 21:07:28 +0200

    drobne zmiany w movielens.py

--- NGCF/data_sets/movielens.py
+++ NGCF/data_sets/movielens.py
@@ -6,24 +6,28 @@ def load_and_process_movielens(file_path, proc_danych):
     print(f"Wczytywanie pliku CSV MovieLens: {file_path}")
     df = pd.read_csv(file_path)
 
+    user_col = 'userId'
+    item_col = 'movieId'
+    rating_col = 'rating'
+
     if proc_danych<1:
-        print("redukcja danych o", proc_danych*100, "%")
-        unique_users = df['userId'].unique()
+        print(f"Redukcja danych o", proc_danych*100, "%")
+        unique_users = df[user_col].unique()
         
         # Wybieramy losowe 25% użytkowników
         sampled_users = np.random.choice(unique_users, size=int(len(unique_users) * proc_danych), replace=False)
         
         # Zostawiamy TYLKO tych użytkowników, ale z CAŁĄ ich historią
-        df = df[df['userId'].isin(sampled_users)].copy()
+        df = df[df[user_col].isin(sampled_users)].copy()
     
     # zostawiamy tylko oceny wieksze od 4 bo wtedy mamy takie realne zainteresowanie czyms
-    df = df[df['rating'] >= 4.0].copy()
+    df = df[df[rating_col] >= 4.0].copy()
 
     # remapping zeby zamiast id=1928372 bylo 1, 2, 3 itd
     user_encoder = LabelEncoder()
     item_encoder = LabelEncoder()
-    df['user_id_idx'] = user_encoder.fit_transform(df['userId'])
-    df['item_id_idx'] = item_encoder.fit_transform(df['movieId'])
+    df['user_id_idx'] = user_encoder.fit_transform(df[user_col])
+    df['item_id_idx'] = item_encoder.fit_transform(df[item_col])
 
     n_users = df['user_id_idx'].nunique() #ile mamy unikalnych ziutków
     n_items = df['item_id_idx'].nunique()
